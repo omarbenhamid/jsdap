c4b9cfec333f81a2fbccbbd550e8685eef32abe4
Omar BENHAMID <omar-externe.benhamid@edf.fr>
2014-01-15 15:11:10 +0100

    Add the possibility to send extra headers

--- jsdap.js
+++ jsdap.js
@@ -1,4 +1,4 @@
-function proxyUrl(url, callback, binary) {
+function proxyUrl(url, callback, binary, extraheaders) {
     // Mozilla/Safari/IE7+
     if (window.XMLHttpRequest) {
         var xml = new XMLHttpRequest();
@@ -12,6 +12,11 @@ function proxyUrl(url, callback, binary) {
         xml.overrideMimeType('text/plain; charset=x-user-defined');
     } else {
         xml.setRequestHeader('Accept-Charset', 'x-user-defined');
+    }
+    if(extraheaders) {
+	    for(var key in extraheaders) {
+		xml.setRequestHeader(key,extraheaders[key]);
+	    }
     }
 
     xml.onreadystatechange = function() {
@@ -30,11 +35,11 @@ function proxyUrl(url, callback, binary) {
 
 /**
  * Load the dataset and call the callback with (dataset) where dataset is the dataset "metadata";
- * The URL should be a raw URL without query parameters : the process with append .dds to it.
+ * - url (string): the url (must be a bare OPeNDAP url, without "format extension" nor query parameters). 
+ * - callback (function(data)): the callback which will receive parsed data.
+ * - extraheaders (map/object) : Javascript Object or map that contains keys and values of additonnal headers for the request.
  */
-function loadDataset(url, callback, proxy) {
-    // User proxy?
-    if (proxy) url = proxy + '?url=' + encodeURIComponent(url);
+function loadDataset(url, callback, extraheaders) {
 
     // Load DDS.
     proxyUrl(url + '.dds', function(dds) {
@@ -44,8 +49,8 @@ function loadDataset(url, callback, proxy) {
         proxyUrl(url + '.das', function(das) {
             dataset = new dasParser(das, dataset).parse();
             callback(dataset);
-        });
-    });
+        },false,extraheaders);
+    },false,extraheaders);
 }
 
 /** Flatten the data array as data attributes of elements of dapvar */
@@ -62,11 +67,13 @@ function _applydata(data,dapvar) {
 }
 /**
  * Load the dataset and call the callback with (data) where data is an array of data
- * the url must be a url with .dods
+ * the url must be a url with .dods extension.
+ * @params:
+ * - url (string): the url (must be a .dods url, it might have additonnal slicing OpENDAP query string)
+ * - callback (function(data)): the callback which will receive parsed data.
+ * - extraheaders (map/object) : Javascript Object or map that contains keys and values of additonnal headers for the request.
  */
-function loadData(url, callback, proxy) {
-    // User proxy?
-    if (proxy) url = proxy + '?url=' + encodeURIComponent(url);
+function loadData(url, callback, extraheaders) {
 
     proxyUrl(url, function(dods) {
         var dds = '';
@@ -81,5 +88,5 @@ function loadData(url, callback, proxy) {
         var data = new dapUnpacker(dods, dapvar).getValue();
 	_applydata(data,dapvar);
        callback(dapvar);
-    }, true);
+    }, true, extraheaders);
 }
